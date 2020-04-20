d795848033ef93a0ee1cc6788445610d8697f012
unknown <lj085754@gmail.com>
2020-04-20 18:57:50 -0500

    File now prints 2 random quotes with no awkward space between them

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,9 @@ def primary():
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
-
+  rnd2 = random.randint(0, last)
+  print(quotes[rnd], end='')
+  print(quotes[rnd2], end='')
+  print("I say \"lets end it here with actual quotes\"")
 if __name__== "__main__":
   primary()
